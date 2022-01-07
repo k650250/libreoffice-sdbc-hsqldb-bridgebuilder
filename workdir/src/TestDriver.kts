c3dd6538598ab75ad01b3a06fc3154550e71acce
北川一樹 <42695615+k650250@users.noreply.github.com>
2022-01-08 01:10:19 +0900

    Update TestDriver.kts
    QueryCommand.java の追加絡み

--- workdir/src/TestDriver.kts
+++ workdir/src/TestDriver.kts
@@ -15,11 +15,12 @@ try {
         DriverManager.getConnection(odbFile.toUrl(), "sa", "").use { conn ->
             conn.createStatement().use { st ->
                 var sql: String
-
+                
                 println("[更新前のデータ一覧]")
-                //sql = """SELECT * FROM "t_sample" """    // テーブル
-                //sql = """SELECT * FROM "v_sample" """    // ビュー
-                sql = odbFile.getQueryCommand("q_sample")  // クエリー
+                //sql = """SELECT * FROM "t_sample" """  // テーブル
+                //sql = """SELECT * FROM "v_sample" """  // ビュー
+                //sql = odbFile.getQueryCommand("q_sample").toString()         // クエリー (問合せ文中にクエリー名が含まれない場合)
+                sql = odbFile.getQueryCommand("q_SAMPLE").expand().toString()  // クエリー (問合せ文中にクエリー名が含まれる場合)
                 st.executeQuery(sql).use { records ->
                     while (records.next()) {
                         println("${records.getString("key")}\t${records.getString("value")}")
@@ -52,6 +53,7 @@ try {
     System.err.println("考えられる原因:")
     System.err.println("- hsqldb.jar が正常に読み込まれていない")
     System.err.println("- このデータベースを別のプロセスが使用中")
+    System.err.println("- 問合せ文中にクエリー名が含まれる")
 } catch (e: Exception) {
     e.printStackTrace()
 }
