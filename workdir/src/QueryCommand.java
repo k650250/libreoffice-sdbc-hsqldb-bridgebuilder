80e1775845d341e1fb30004575ef849712dddef5
北川一樹 <42695615+k650250@users.noreply.github.com>
2022-01-08 00:49:44 +0900

    Create QueryCommand.java

---
+++ workdir/src/QueryCommand.java
@@ -0,0 +1,64 @@
+// -*- coding: UTF-8; -*-
+
+package com.k650250.odb;
+
+import com.k650250.odb.ODBFile;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class QueryCommand {
+    protected String command;
+    protected ODBFile odbFile;
+
+    /* コンストラクタ
+     */
+    QueryCommand(String command, ODBFile odbFile) {
+        this.command = command;
+        this.odbFile = odbFile;
+    }
+
+    /* インスタンス・メソッド
+     * 文字列表現に
+     */
+    @Override
+    public String toString() {
+        return this.command;
+    }
+
+    /* インスタンス・メソッド
+     * 問合せ文中の一切のクエリー名をそれが指し示す副問合せに置換
+     */
+    public QueryCommand expand() {
+        if (this.odbFile.isClosed()) {
+            return this;
+        }
+
+        final String BEGIN_NAME = "\"";
+        final String END_NAME = "\"";
+        final String BEGIN_COMMAND = "( ";
+        final String END_COMMAND = " )";
+        final String QUERY_TAG = "db:query";
+        final String NAME_ATTR = "db:name";
+        final String COMMAND_ATTR = "db:command";
+        final Element root = this.odbFile.content.getDocumentElement();
+        final NodeList queries = root.getElementsByTagName(QUERY_TAG);
+        
+        String oldCommand;
+        String newCommand = this.command;
+
+        do {
+            oldCommand = String.copyValueOf(newCommand.toCharArray());
+
+            for (int i = 0; i < queries.getLength(); i++) {
+                Element query = (Element)queries.item(i);
+
+                newCommand = newCommand.replace(
+                    BEGIN_NAME + query.getAttribute(NAME_ATTR) + END_NAME,
+                    BEGIN_COMMAND + query.getAttribute(COMMAND_ATTR) + END_COMMAND
+                );
+            }
+        } while (!newCommand.equals(oldCommand));
+
+        return new QueryCommand(newCommand, this.odbFile);
+    }
+}
