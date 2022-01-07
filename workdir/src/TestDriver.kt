2ace75b92a5de1ff6e654adfd2f9f8cb9b87d7ea
北川一樹 <42695615+k650250@users.noreply.github.com>
2022-01-08 01:06:57 +0900

    Update TestDriver.kt
    QueryCommand.java の追加絡み

--- workdir/src/TestDriver.kt
+++ workdir/src/TestDriver.kt
@@ -21,9 +21,10 @@ fun main() {
             var records: ResultSet
 
             println("[更新前のデータ一覧]")
-            //sql = """SELECT * FROM "t_sample" """    // テーブル
-            //sql = """SELECT * FROM "v_sample" """    // ビュー
-            sql = odbFile.getQueryCommand("q_sample")  // クエリー
+            //sql = """SELECT * FROM "t_sample" """  // テーブル
+            //sql = """SELECT * FROM "v_sample" """  // ビュー
+            //sql = odbFile.getQueryCommand("q_sample").toString()         // クエリー (問合せ文中にクエリー名が含まれない場合)
+            sql = odbFile.getQueryCommand("q_SAMPLE").expand().toString()  // クエリー (問合せ文中にクエリー名が含まれる場合)
             records = st.executeQuery(sql)
             while (records.next()) {
                 println("${records.getString("key")}\t${records.getString("value")}")
@@ -57,6 +58,7 @@ fun main() {
         System.err.println("考えられる原因:")
         System.err.println("- hsqldb.jar が正常に読み込まれていない")
         System.err.println("- このデータベースを別のプロセスが使用中")
+        System.err.println("- 問合せ文中にクエリー名が含まれる")
     } catch (e: Exception) {
         e.printStackTrace()
     }
