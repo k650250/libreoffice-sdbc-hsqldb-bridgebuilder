231f315fabac88add803b9457c053f6b4f6067e2
北川一樹 <42695615+k650250@users.noreply.github.com>
2022-01-11 15:21:01 +0900

    Update TestDriver.py
    自動クローズ処理法を contextlib.closing に変更

--- workdir/src/TestDriver.py
+++ workdir/src/TestDriver.py
@@ -1,14 +1,8 @@
 # -*- coding: UTF-8; -*-
 
-from contextlib import contextmanager
-@contextmanager
-def use(closeableObject):
-    try:
-        yield closeableObject
-    finally:
-        closeableObject.close()
 import sys
 import traceback
+from contextlib import closing
 from com.k650250.odb import ODBFile
 from java.lang import Class, ClassNotFoundException, System
 from java.sql import DriverManager, SQLException
@@ -20,28 +14,28 @@ from java.util import Scanner
 try:
     Class.forName("org.hsqldb.jdbcDriver")
 
-    with use(ODBFile.open("sample.odb")) as odbFile:
-        with use(DriverManager.getConnection(odbFile.toUrl(), "sa", "")) as conn:
-            with use(conn.createStatement()) as st:
+    with closing(ODBFile.open("sample.odb")) as odbFile:
+        with closing(DriverManager.getConnection(odbFile.toUrl(), "sa", "")) as conn:
+            with closing(conn.createStatement()) as st:
                 print(u"[更新前のデータ一覧]")
                 #sql = 'SELECT * FROM "t_sample"'  # テーブル
                 #sql = 'SELECT * FROM "v_sample"'  # ビュー
                 #sql = odbFile.getQueryCommand("q_sample").toString()          # クエリー (問合せ文中にクエリー名が含まれない場合)
                 sql = odbFile.getQueryCommand("q_SAMPLE").expand().toString()  # クエリー (問合せ文中にクエリー名が含まれる場合)
-                with use(st.executeQuery(sql)) as records:
+                with closing(st.executeQuery(sql)) as records:
                     while records.next():
                         print(u"{}\t{}".format(records.getString("key"), records.getString("value")))
 
                 # データ挿入
                 sql = 'INSERT INTO "t_sample"("value") VALUES(?)'
-                with use(conn.prepareStatement(sql)) as prep:
+                with closing(conn.prepareStatement(sql)) as prep:
                     System.out.print(u"追加データを入力してください: ")
                     prep.setString(1, Scanner(System.in).nextLine())
                     prep.executeUpdate()
 
                 print(u"[更新後のデータ一覧]")
                 sql = 'SELECT * FROM "t_sample"'
-                with use(st.executeQuery(sql)) as records:
+                with closing(st.executeQuery(sql)) as records:
                     while records.next():
                         print(u"{}\t{}".format(records.getString("key"), records.getString("value")))
 
