59c512b431bfcc00803cc77d2bb573d5a3810873
北川一樹 <42695615+k650250@users.noreply.github.com>
2023-01-23 15:42:45 +0900

    `conn`->`con`等

--- workdir/src/TestDriver.py
+++ workdir/src/TestDriver.py
@@ -15,38 +15,39 @@ try:
     Class.forName("org.hsqldb.jdbcDriver")
 
     with closing(ODBFile.open("sample.odb")) as odbFile:
-        with closing(DriverManager.getConnection(odbFile.toUrl(), "sa", "")) as conn:
-            with closing(conn.createStatement()) as st:
-                print(u"[更新前のデータ一覧]")
+        with closing(DriverManager.getConnection(odbFile.toUrl(), "sa", "")) as con:
+            with closing(con.createStatement()) as st:
+                System.out.println(u"[更新前のデータ一覧]")
                 #sql = 'SELECT * FROM "t_sample"'  # テーブル
                 #sql = 'SELECT * FROM "v_sample"'  # ビュー
                 #sql = odbFile.getQuery("q_sample").toString()          # クエリー (問合せ文中にクエリー名が含まれない場合)
                 sql = odbFile.getQuery("q_SAMPLE").expand().toString()  # クエリー (問合せ文中にクエリー名が含まれる場合)
                 with closing(st.executeQuery(sql)) as records:
                     while records.next():
-                        print(u"{}\t{}".format(records.getString("key"), records.getString("value")))
+                        System.out.println(u"{}\t{}".format(records.getString("key"), records.getString("value")))
 
                 # データ挿入
                 sql = 'INSERT INTO "t_sample"("value") VALUES(?)'
-                with closing(conn.prepareStatement(sql)) as prep:
-                    System.out.print(u"追加データを入力してください: ")
-                    prep.setString(1, Scanner(System.in).nextLine())
-                    prep.executeUpdate()
+                with closing(con.prepareStatement(sql)) as prep:
+                    with closing(Scanner(eval("System.in"), System.getProperty("native.encoding"))) as sc:
+                        System.out.print(u"追加データを入力してください: ")
+                        prep.setString(1, sc.nextLine())
+                        prep.executeUpdate()
 
-                print(u"[更新後のデータ一覧]")
+                System.out.println(u"[更新後のデータ一覧]")
                 sql = 'SELECT * FROM "t_sample"'
                 with closing(st.executeQuery(sql)) as records:
                     while records.next():
-                        print(u"{}\t{}".format(records.getString("key"), records.getString("value")))
+                        System.out.println(u"{}\t{}".format(records.getString("key"), records.getString("value")))
 
 except ClassNotFoundException as e:
     e.printStackTrace()
-    print >> sys.stderr, u"org.hsqldb.jdbcDriver が見つかりません。"
+    System.err.println(u"org.hsqldb.jdbcDriver が見つかりません。")
 except SQLException as e:
     e.printStackTrace()
-    print >> sys.stderr, u"考えられる原因:"
-    print >> sys.stderr, u"- hsqldb.jar が正常に読み込まれていない"
-    print >> sys.stderr, u"- このデータベースを別のプロセスが使用中"
-    print >> sys.stderr, u"- 問合せ文中にクエリー名が含まれる"
+    System.err.println(u"考えられる原因:")
+    System.err.println(u"- hsqldb.jar が正常に読み込まれていない")
+    System.err.println(u"- このデータベースを別のプロセスが使用中")
+    System.err.println(u"- 問合せ文中にクエリー名が含まれる")
 except Exception as e:
     traceback.print_exc(file=sys.stderr)
